3213516b21a2a81cdcae478c7f08c4af9644174f
abanguttar <uttarpradeshnahendra@hotmail.com>
2024-11-20 12:35:37 +0700

    Fixed Controller

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Kelas;
-use App\Models\KelasDetail
+use App\Models\KelasDetail;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\KelasKategori;

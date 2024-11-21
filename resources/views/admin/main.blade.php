b9697f957a39823daee5b2928c0361bc4aca047e
abanguttar <uttarpn88@gmail.com>
2024-11-21 01:01:19 -0800

    Add logo to login admin

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/feather-icons"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
 
     @stack('link')
@@ -37,7 +38,9 @@
             </aside>
         @endif
         <div class="container-xl mt-5">
+            @if ($title !== 'Login' && $title !== 'Register')
             <h3 class="text-center">{{ $title }}</h3>
+            @endif
             @yield('body')
         </div>
     </main>

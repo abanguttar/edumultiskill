075056c2d7636fc6e1047c14f5f39921c3dcebca
abanguttar <130395778+abanguttar@users.noreply.github.com>
2024-12-11 20:57:07 +0700

    Admin/faq (#40)
    * FAQ Fixed with FAQSeeder
    
    * Update date in faq member

--- resources/views/member/faq/index.blade.php
+++ resources/views/member/faq/index.blade.php
@@ -15,7 +15,27 @@
         <div class="container-xl d-flex flex-column mt-4 pt-3 gap-2">
             <div>
                 <h1>{{ ucwords($title) }}</h1>
-                <span class="text-primary fw-bold">Diperbaharui {{ date('d m Y', strtotime($faq->updated_at)) }}</span>
+                @php
+
+                    $bulan = [
+                        1 => 'Januari',
+                        'Februari',
+                        'Maret',
+                        'April',
+                        'Mei',
+                        'Juni',
+                        'Juli',
+                        'Agustus',
+                        'September',
+                        'Oktober',
+                        'November',
+                        'Desember',
+                    ];
+                    $date = date('d-m-Y', strtotime($faq->updated_at));
+                    $exp_tanggal = explode('-', $date);
+                    $tanggal = implode(' ', [$exp_tanggal[0], $bulan[$exp_tanggal[1]], $exp_tanggal[2]]);
+                @endphp
+                <span class="text-primary fw-bold">Diperbaharui {{ $tanggal }}</span>
             </div>
 
             <div>

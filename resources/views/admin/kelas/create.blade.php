7d061cdc4cd1aa14c20a97967435b5558c12e4bd
abanguttar <uttarpn88@gmail.com>
2024-12-13 18:14:50 +0700

    fix kelas/create button informasi

--- resources/views/admin/kelas/create.blade.php
+++ resources/views/admin/kelas/create.blade.php
@@ -307,8 +307,8 @@
 
     <script>
         $(document).ready(function() {
-            $(`#btn-group-${@json($btn_group)}`).removeClass('btn-outline-danger').addClass(
-                'btn-danger');
+            $(`#btn-group-${@json($btn_group)}`).removeClass('btn-outline-primer').addClass(
+                'btn-primer');
         })
     </script>
     <script>

8d6b037c86802261c0f2dfebb846443e3c6987b9
abanguttar <uttarpn88@gmail.com>
2024-12-20 18:25:51 +0700

    Update Companty Profile

--- resources/views/components/scrollable-box-instruktur.blade.php
+++ resources/views/components/scrollable-box-instruktur.blade.php
@@ -4,10 +4,10 @@
             @foreach ($tutors as $tutor)
                 <div class="d-flex flex-column align-items-center position-relative col-12 col-md-6 col-xl-4 p-3">
 
-                    <figure class="w-100 d-flex justify-content-center rounded-top-5 pt-3 pb-3"
+                    <figure class="w-100 d-flex justify-content-center align-items-end rounded-top-5 pt-3 "
                         style="background-image: url('main-assets/bg-tutor.webp');  background-position: center;">
                         <img src="{{ $tutor->foto === null ? '/main-assets/mock-image.webp' : '/user-image/' . $tutor->foto }}"
-                            alt="sample" width="200px">
+                            alt="sample" height="250px" style="object-fit: contain" width="200px">
                     </figure>
                     <div
                         class="mt-2 d-flex flex-column align-items-center justify-content-between rounded-bottom-5 border-bottom border-3 w-100">
